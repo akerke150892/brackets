c347591836b2b64891c994935185a5e5a48fc3ff
aksss15 <akerke.otarbayeva@gmail.com>
2021-03-15 23:31:56 +0600

    fix: fix check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = [];
+  for (let elem of bracketsConfig) {
+    newArr.push(elem.join(""));
+  }
+  
+  let stack = "";
+  for (const char of str) {
+    const lastCharInStack = stack[stack.length-1];
+    const twoChars = lastCharInStack + char;
+    if (newArr.includes(twoChars))
+      stack = stack.slice(0, -1);
+    else
+      stack = stack + char;
+  }
+  return !stack;
 }
